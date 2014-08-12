e0e442b4d15ab1067c47fdbc565e605d703366c2
Mars Hsu <marshsu@gmail.com>
2014-08-12 16:32:59 +0800

    1. images upload use modal 2. on change add "has-change" to body and the item

--- javascripts/v4/domready.app.brand.js
+++ javascripts/v4/domready.app.brand.js
@@ -8,14 +8,56 @@ $(function () {
     var $page = cms['app-brand'],
         $common = cms.common;
 
+    $(document).on("click", "#funcCancel", function (event) {
+        
+
+        $('#imageUpload').modal('show');
+    });
+
+    $(document).on("click", ".logoSNS", function (event) {
+        var thisSNS = $(this).parent().parent().parent().parent(),
+            snsId = thisSNS.data("meta"),
+            parameter = {
+                'prefix': 'app-brand-SNS-',
+                'type': 'image',
+                'size': 512000,
+                'acl': 'public-read'
+            };
+
+        $common.showProcessingOverlay();
+
+        $("#imageUpload").data("meta", thisSNS.attr("id"));
+        $("#modalUpload").html("<p id='uploadThumbnail'></p>");
+
+        $page.imageUpload("SNS", parameter);
+    });
+
+
+    $(document).on("change", ".inLinkSugg, .inTitleSugg", function (event) {
+        var thisSugg = $(this).parent().parent().parent().parent();
+
+        thisSugg.addClass("has-change");
+        $("body").addClass("has-change");
+    });
+
+
     $(document).on("change", ".inUrlSNS", function (event) {
-        var thisVal = $(this).val().trim(),
-            msgBox = $(this).parent().parent().find("span.msgSNS");
+        var thisSNS = $(this).parent().parent().parent(),
+            thisVal = $(this).val().trim(),
+            msgBox = $(this).parent().parent().find("span.msgSNS"),
+            logoBox = $(this).parent().find("a.logoSNS"),
+            iconImg = "";
+
+        thisSNS.addClass("has-change");
+        $("body").addClass("has-change");
 
         $(this).val(thisVal);
-        if (thisVal !== "") {
+        iconImg = $page.changeUrlSNS(thisVal);
+        if (iconImg !== "") {
+            logoBox.html("<img class='logoUrl' src='" + iconImg + "' width='30px' height-'30px' >");
             msgBox.addClass("hide");
         } else {
+            logoBox.html("<span class='glyphicon glyphicon-cloud-upload'></span>");
             msgBox.removeClass("hide");
         }
     });
@@ -57,25 +99,78 @@ $(function () {
             if (itemCount.itemCount >= $page.limitSNS) {
                 $("#addNewSNS").addClass("hide");
             }
+            $('[data-toggle=popover]').popover({
+                html: true,
+                trigger: 'hover'
+
+            });
         }
     });
 
-    $(document).on("click", "#addNewSuggested", function(event) {
+    $(document).on("click", ".uploadSugg", function (event) {
+
+        // $('#imageUpload').modal('show');
+        // nn.log(".uploadSugg .uploadSugg . uploadSugg. uploadSugg. uploadSugg");
+
+        var thisSNS = $(this).parent().parent(),
+            snsId = thisSNS.data("meta"),
+            parameter = {
+                'prefix': 'app-brand-Suggested-',
+                'type': 'image',
+                'size': 5120000,
+                'acl': 'public-read'
+            };
+
+        $common.showProcessingOverlay();
+
+        $("#imageUpload").data("meta", thisSNS.attr("id"));
+        $("#modalUpload").html("<p id='uploadThumbnail'></p>");
+
+        $page.imageUpload("Sugg", parameter);
+
+
+
+    });
+
+    $(document).on("click", ".delSugg", function (event) {
+        var opObj = $(this).parent().parent().parent(),
+            itemCount = $page.itemCountSugg();
+
+        if (opObj.hasClass("newItem")) {
+            opObj.remove();
+        } else {
+            opObj.addClass("hide").addClass("delItem");
+        }
+
+        itemCount = $page.itemCountSNS();
+
+        if (itemCount.itemCount >= $page.limitSuggested) {
+            $("#addNewSuggested").addClass("hide");
+        } else {
+            $("#addNewSuggested").removeClass("hide");
+        }
+    });
+
+    $(document).on("click", "#addNewSuggested", function (event) {
         var emptyItem = {
             "id": new Date().getTime(),
             "type": 2,
             "seq": 0,
             "title": "",
             "link": "",
-            "logoUrl": ""
+            "logoUrl": $page.defImgSugg,
+            "isSecII": false
         },
             itemCount = $('#listsSuggested .listItem').length;
 
+        if (itemCount >= 4) {
+            emptyItem.isSecII = true;
+        }
 
         if ($page.limitSuggested > itemCount) {
             $('#tmpl-lists-Suggested').tmpl(emptyItem, null).appendTo('#listsSuggested');
-            itemCount ++;
-            if(itemCount >= $page.limitSuggested){
+            itemCount++;
+            if (itemCount >= $page.limitSuggested) {
                 $("#addNewSuggested").addClass("hide");
             }
         }
@@ -94,7 +189,6 @@ $(function () {
             });
         }
 
-        nn.log(inMsoInfo);
         return false;
     });
 
--- javascripts/v4/func.app.brand.js
+++ javascripts/v4/func.app.brand.js
@@ -11,15 +11,143 @@
     $page.limitSNS = 4;
     $page.limitSuggestedMin = 4;
     $page.limitSuggested = 6;
+    $page.defImgSNS = "http://fakeimg.pl/100/";
+    $page.defImgSugg = "http://fakeimg.pl/200/";
+
+    $page.imageUpload = function (inType, parameter) {
+ 
+        var thisId = $("#imageUpload").data("meta");
+        nn.api('GET', cms.reapi('/api/s3/attributes'), parameter, function (s3attr) {
+            var timestamp = (new Date()).getTime(),
+                handlerUploadProgress = function (file, completed, total) {
+                    this.setButtonText('<span class="uploadstyle">' + nn._(['upload', 'Uploading...']) + '</span>');
+                },
+                handlerUploadSuccess = function (file, serverData, recievedResponse) {
+                    this.setButtonText('<span class="uploadstyle">' + nn._(['upload', 'Upload']) + '</span>');
+                    if (!file.type) {
+                        file.type = nn.getFileTypeByName(file.name);
+                    }
+                    this.setButtonDisabled(false);
+                    // enable upload button again
+                    var url = 'http://' + s3attr.bucket + '.s3.amazonaws.com/' + parameter.prefix + timestamp + '-' + file.size + file.type.toLowerCase();
+                    $('body').addClass("has-change");
+                    $("#" + thisId).addClass("has-change");
+                    $("#" + thisId).find("img.logoUrl").attr("src", url);
+                    setTimeout(function() {
+                        $('#imageUpload').modal('hide');
+                    }, 500);
+                },
+                handlerUploadError = function (file, code, message) {
+
+                    this.setButtonText('<span class="uploadstyle">' + nn._(['upload', 'Upload']) + '</span>');
+                    this.setButtonDisabled(false);
+                    if (code === -280) { // user cancel upload
+                        alert(message);
+                        // show some error prompt
+                    } else {
+                        alert(message);
+                        // show some error prompt
+                    }
+                },
+                handlerFileQueue = function (file) {
+                    if (file.size > parameter.size) {
+                        // $common.showSystemErrorOverlay(nn._([cms.global.PAGE_ID, 'brand-layer', 'your upload file is large than 500KB, please reupload file.']));
+                        alert("upload failed");
+                        return false;
+                    }
+                    if (!file.type) {
+                        file.type = nn.getFileTypeByName(file.name);
+                        // Mac Chrome compatible
+                    }
+                    var postParams = {
+                        "AWSAccessKeyId": s3attr.id,
+                        "key": parameter.prefix + timestamp + '-' + file.size + file.type.toLowerCase(),
+                        "acl": parameter.acl,
+                        "policy": s3attr.policy,
+                        "signature": s3attr.signature,
+                        "content-type": parameter.type,
+                        "success_action_status": "201"
+                    };
+                    this.setPostParams(postParams);
+                    this.startUpload(file.id);
+                    this.setButtonDisabled(true);
+                },
+                handlerFileQueueError = function (file, code, message) {
+                    if (code === -130) { // error file type
+                        // $('#brand-logo').removeClass('hide');
+                        // $('.img .loading').hide();
+                    }
+                },
+                settings = {
+                    flash_url: 'javascripts/libs/swfupload/swfupload.swf',
+                    upload_url: 'http://' + s3attr.bucket + '.s3.amazonaws.com/', // http://9x9tmp-ds.s3.amazonaws.com/
+                    file_size_limit: parameter.size,
+                    file_types: '*.png',
+                    file_types_description: 'Thumbnail',
+                    file_post_name: 'file',
+                    button_placeholder: $('#uploadThumbnail').get(0),
+                    button_image_url: 'images/btn-load.png',
+                    button_width: '129',
+                    button_height: '29',
+                    button_text: '<span class="uploadstyle">' + nn._(['upload', 'Upload']) + '</span>',
+                    button_text_style: '.uploadstyle { color: #777777; font-family: Arial, Helvetica; font-size: 15px; text-align: center; } .uploadstyle:hover { color: #999999; }',
+                    button_text_top_padding: 1,
+                    button_action: SWFUpload.BUTTON_ACTION.SELECT_FILE,
+                    button_cursor: SWFUpload.CURSOR.HAND,
+                    button_window_mode: SWFUpload.WINDOW_MODE.TRANSPARENT,
+                    http_success: [201],
+                    upload_progress_handler: handlerUploadProgress,
+                    upload_success_handler: handlerUploadSuccess,
+                    upload_error_handler: handlerUploadError,
+                    file_queued_handler: handlerFileQueue,
+                    file_queue_error_handler: handlerFileQueueError,
+                    debug: false
+                },
+                swfu = new SWFUpload(settings);
+
+            swfu.debug = cms.config.IS_DEBUG;
+            $common.hideProcessingOverlay();
+            $('#imageUpload').modal('show');
+        });
+    };
+
+    $page.changeUrlSNS = function (inUrl) {
+        var inURL = $.url(inUrl),
+            strHost = inURL.attr("host"),
+            retValue = "";
+        switch (strHost) {
+            case "www.facebook.com":
+                retValue = "images/icon-pcs-sns-fb.png";
+                break;
+            case "plus.google.com":
+                retValue = "images/icon-pcs-sns-gplus.png";
+
+                break;
+            case "www.youtube.com":
+                retValue = "images/icon-pcs-sns-youtube.png";
+
+                break;
+            case "twitter.com":
+                retValue = "images/icon-pcs-sns-twitter.png";
+                break;
+        }
+        return retValue;
+    };
+
 
     $page.itemCountSNS = function () {
         var retValue = {
             delCount: $('#listsSNS .listItem.delItem').length,
             itemCount: $('#listsSNS .listItem').length - $('#listsSNS .listItem.delItem').length
         };
+        return retValue;
+    };
 
-        // retValue.itemCount = retValue.itemCount - retValue.delCount;
-
+    $page.itemCountSugg = function () {
+        var retValue = {
+            delCount: $('#listsSuggested .listItem.delItem').length,
+            itemCount: $('#listsSuggested .listItem').length - $('#listsSuggested .listItem.delItem').length
+        };
         return retValue;
     };
 
@@ -86,16 +214,23 @@
             msoId: cms.global.MSO
         }), {
             type: 2
-        }, function (dataLists) {
-            nn.log("SNS count["+dataLists.length+"]");
+        }, function(dataLists) {
+            $.each(dataLists, function(eKey, eValue) {
+                if("" == eValue.logoUrl){
+                    eValue.logoUrl = $page.defImgSugg;
+                }
+                if (eKey < $page.limitSuggestedMin) {
+                    eValue.isSecII = false;
+                } else {
+                    eValue.isSecII = true;
+                }
+                dataLists[eKey] = eValue;
+            });
 
+            $('#tmpl-lists-Suggested').tmpl(dataLists, null).appendTo('#listsSuggested');
 
-                    $('#tmpl-lists-Suggested').tmpl(dataLists, null).appendTo('#listsSuggested');
-
-
-                        $page.isSuggested = true;
-                        $page.chkFormSet();
-
+            $page.isSuggested = true;
+            $page.chkFormSet();
         });
     };
 
