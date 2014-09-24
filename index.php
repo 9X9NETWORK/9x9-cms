e396d7a62ca34f8fb44a0067d2f00b651284ab94
Louis Jeng <louisje@gmail.com>
2014-09-24 07:01:09 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+    $host = $_SERVER['HTTP_HOST'];
+    list($msoName, $remain) = explode('.', $host, 2);
+    $input = isset($_GET['input']) ? $_GET['input'] : 'index.html';
+
+    $content = file_get_contents(__DIR__ . '/' . $input);
+
+    $mso = json_decode(file_get_contents("http://localhost:8080/api/mso/$msoName"), true);
+    if ($mso != null) {
+
+        if ($mso['jingleUrl']) {
+
+            $content = str_replace('href="favicon_flipr_pg.ico"',
+                                   'href="' . htmlspecialchars($mso['jingleUrl']) . '"', $content);
+        }
+
+        if ($mso['cmsLogo']) {
+
+            $content = str_replace('src="images/logo_flipr_pg.png"',
+                                   'src="' . htmlspecialchars($mso['cmsLogo']) . '"', $content);
+        }
+    }
+
+    header('Content-Type: text/html');
+    header('Content-Length: ' . strlen($content));
+
+    echo $content;
+
